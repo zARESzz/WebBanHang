541691aadce85db1c5fd4c367396477ecc011de4
LeonRoy06 <114150434+LeonRoy06@users.noreply.github.com>
2023-04-02 20:26:28 +0700

    GiaoDienTrangChu1

---
+++ WebBanHang/Controllers/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class MenuController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MenuTop()
+        {
+            var items = db.Categories.OrderBy(x=>x.Position).ToList();
+            return PartialView("_MenuTop",items);
+        }
+        public ActionResult MenuProductCategory()
+        {
+            var item = db.ProductCategories.ToList();
+            return PartialView("_MenuProductCategory",item);
+        }
+        public ActionResult Menuarrivals()
+        {
+            var item = db.ProductCategories.ToList();
+            return PartialView("_Menuarrivals", item);
+        }
+    }
+}
---
+++ WebBanHang/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        public ActionResult Partial_ItemsByCateId()
+        {
+            var item = db.Products.Where(x => x.IsActive).ToList();
+            return PartialView("Partial_ItemsByCateId", item);
+        }
+        public ActionResult GetCategory() 
+        { 
+            var context = new ApplicationDbContext();
+            var listCategory = context.Categories.ToList(); 
+            return PartialView(listCategory);
+        }
+        public ActionResult GetProductByCategory(int id)
+        {
+            var context = new ApplicationDbContext();
+            return View("Index", context.Products.Where(p => p.ProductCategoryID == id).ToList());
+        }
+    }
+}
