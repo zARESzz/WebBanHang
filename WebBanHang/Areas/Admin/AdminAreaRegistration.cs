8e2d73484b10f155ea2c6ccbf7dbdf3a899e10d5
LeonRoy06 <114150434+LeonRoy06@users.noreply.github.com>
2023-04-04 23:58:14 +0700

    Web API

--- WebBanHang/Areas/Admin/AdminAreaRegistration.cs
+++ WebBanHang/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace WebBanHang.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional },
+                new { action = "Index",id = UrlParameter.Optional },
                  namespaces: new[] { "WebBanHang.Areas.Admin.Controllers" }
             );
           
