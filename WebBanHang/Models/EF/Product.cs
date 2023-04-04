b945e9ae43f91eabab1bb264e3ca34dc9e1b5af5
TuanAnh2401 <lta24012k2@gmail.com>
2023-04-04 23:48:18 +0700

    cmt

--- WebBanHang/Models/EF/Product.cs
+++ WebBanHang/Models/EF/Product.cs
@@ -28,7 +28,7 @@ namespace WebBanHang.Models.EF
         [AllowHtml]
         public string Detail { get; set; }
         public string Image { get; set; }
-
+        public decimal? OriginalPrice { get; set; }
         public decimal Price { get; set; }
         public decimal? PriceSale { get; set; }
         public int Quantity { get; set; }
