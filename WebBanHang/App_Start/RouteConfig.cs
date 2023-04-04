a66736be942bc65496edaa39e516ab735c5466c8
TuanAnh2401 <lta24012k2@gmail.com>
2023-04-04 20:22:36 +0700

    Fix

--- WebBanHang/App_Start/RouteConfig.cs
+++ WebBanHang/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebBanHang
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                 namespaces: new[] {"WebBanHang.Controllers"}
             );
             routes.MapRoute(
