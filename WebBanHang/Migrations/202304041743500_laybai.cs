679fbee4a2254bd0f89a54231798628f057da25b
zARESzz <phamthehung1912@gmail.com>
2023-04-05 08:58:22 +0700

    ABC

---
+++ WebBanHang/Migrations/202304041743500_laybai.cs
@@ -0,0 +1,20 @@
+﻿namespace WebBanHang.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class laybai : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tb_Product", "OriginalPrice", c => c.Decimal(precision: 18, scale: 2));
+            AlterColumn("dbo.tb_Contact", "Website", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.tb_Contact", "Website", c => c.String());
+            DropColumn("dbo.tb_Product", "OriginalPrice");
+        }
+    }
+}
---
+++ WebBanHang/Migrations/202304041748573_laydata2.cs
@@ -0,0 +1,16 @@
+﻿namespace WebBanHang.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class laydata2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
